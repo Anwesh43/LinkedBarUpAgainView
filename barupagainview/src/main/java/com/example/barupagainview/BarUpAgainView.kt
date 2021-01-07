c4a640173ac30d0ec86f2ee2709cd87b20059a3f
Anwesh43 <anweshthecool0@gmail.com>
2021-01-07 13:27:18 +0530

    adding constants and libraries

--- barupagainview/src/main/java/com/example/barupagainview/BarUpAgainView.kt
+++ barupagainview/src/main/java/com/example/barupagainview/BarUpAgainView.kt
@@ -1,2 +1,26 @@
 package com.example.barupagainview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val lineHFactor : Float = 2f
+val barHFactor : Float = 8.9f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#3F51B5",
+    "#795548",
+    "#009688",
+    "#2196F3"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
