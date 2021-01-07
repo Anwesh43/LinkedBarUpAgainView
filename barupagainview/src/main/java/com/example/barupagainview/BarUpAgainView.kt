75845611dbb3b35b48739661cf2600a635319b9d
Anwesh43 <anweshthecool0@gmail.com>
2021-01-07 14:09:25 +0530

    adding BUANode class

--- barupagainview/src/main/java/com/example/barupagainview/BarUpAgainView.kt
+++ barupagainview/src/main/java/com/example/barupagainview/BarUpAgainView.kt
@@ -129,4 +129,45 @@ class BarUpAgainView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BUANode(var i : Int, val state : State = State()) {
+
+        private var next : BUANode? = null
+        private var prev : BUANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BUANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBUANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BUANode {
+            var curr : BUANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
