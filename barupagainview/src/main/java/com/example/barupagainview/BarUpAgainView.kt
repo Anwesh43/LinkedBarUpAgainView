f52b0c3adb6b1505214b56dd46eaf3a8dcb2bc3a
Anwesh43 <anweshthecool0@gmail.com>
2021-01-07 13:39:55 +0530

    adding Canvas extension methods

--- barupagainview/src/main/java/com/example/barupagainview/BarUpAgainView.kt
+++ barupagainview/src/main/java/com/example/barupagainview/BarUpAgainView.kt
@@ -29,3 +29,40 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBarUpAgain(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val lineH : Float = Math.min(w, h) / lineHFactor
+    val barH : Float = Math.min(w, h) / barHFactor
+    save()
+    translate(w / 2, h)
+    for (j in 0..1) {
+        save()
+        translate(0f, -barH / 2 + barH * j)
+        drawLine(0f, 0f, 0f, -(lineH + barH) * sf1, paint)
+        restore()
+    }
+    for (j in 0..1) {
+        save()
+        translate(-barH / 2, -(lineH + barH) + barH * j)
+        drawLine(0f, 0f, barH * sf2, 0f, paint)
+        restore()
+    }
+    save()
+    translate(-barH / 2, -lineH)
+    drawRect(RectF(0f, -barH * sf3, barH, 0f), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawBUANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarUpAgain(scale, w, h, paint)
+}
